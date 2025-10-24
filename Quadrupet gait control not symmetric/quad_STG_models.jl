571d18b046226d214c50d5ff560163c60cdb8d51
Arthur Fyon <arthurfyon@hotmail.be>
2025-10-24 16:01:34 +0200

    Adding new data for asymmetric

--- Quadrupet gait control not symmetric/quad_STG_models.jl
+++ Quadrupet gait control not symmetric/quad_STG_models.jl
@@ -45,6 +45,132 @@ function uve(g, g_r, z, Kp, Ki, u_max)
     return u, v, e
 end
 
+function simulateSTG_network_diff_ICs(Iapp, gNa, gCaT, gCaSinit, gAinit, gKCa, gKd, gH, gleak, gsyn,
+                             tmKCa, α, β, Kp, Ki, Kt, gsth, guth, Vth, u_maxCaS, u_maxA, Vprev)
+
+    # Extracting synaptic conductances
+    gsyn12 = gsyn[1]
+    gsyn21 = gsyn[2]
+    gsyn34 = gsyn[3]
+    gsyn43 = gsyn[4]
+    gsyn51 = gsyn[5]
+    gsyn53 = gsyn[6]
+    gsyn62 = gsyn[7]
+    gsyn63 = gsyn[8]
+
+    # Initial conditions
+    gCaS = copy(gCaSinit)
+    gCaSi = copy(gCaSinit)
+    zCaS = zero(gCaS)
+    gA = copy(gAinit)
+    gAi = copy(gAinit)
+    zA = zero(gA)
+
+    #Vprev = .-60. .* ones(6) .+ 10. .* (rand(6).-0.5)
+    Vcur = copy(Vprev)
+    mNa = mNa_inf.(Vprev)
+    hNa = hNa_inf.(Vprev)
+    mCaT = mCaT_inf.(Vprev)
+    hCaT = hCaT_inf.(Vprev)
+    mCaS = mCaS_inf.(Vprev)
+    hCaS = hCaS_inf.(Vprev)
+    mA = mA_inf.(Vprev)
+    hA = hA_inf.(Vprev)
+    Ca = .-0.94 .* (gCaT .* mCaT.^3 .* hCaT .* (Vprev.-VCa) .+
+                    gCaS .* mCaS.^3 .* hCaS .* (Vprev.-VCa)) .+ 0.05
+    mKCa = mKCa_inf.(Vprev, Ca)
+    mKd = mKd_inf.(Vprev)
+    mH = mH_inf.(Vprev)
+
+    gs_th = zeros(Tdt+1, 6)
+    gu_th = zero(gs_th)
+    for j = 1 : 6
+        gs_th[:, j] = gsth[j].(tsim)
+        gu_th[:, j] = guth[j].(tsim)
+    end
+
+    gCaS_r = zeros(6)
+    gA_r = zeros(6)
+    for j = 1 : 6
+        (gCaS_r[j], gA_r[j]) = DICs_gmax_neuromodCaSA(gNa[j], gCaT[j], gKd[j], gKCa[j], gH[j],
+                                                gleak[j], gs_th[1, j], gu_th[1, j], Vth[j], tmKCa[j])
+    end
+
+    s12 = s_inf(Vprev[1])
+    s21 = s_inf(Vprev[2])
+    s34 = s_inf(Vprev[3])
+    s43 = s_inf(Vprev[4])
+    s51 = s_inf(Vprev[5])
+    s53 = s_inf(Vprev[5])
+    s62 = s_inf(Vprev[6])
+    s63 = s_inf(Vprev[6])
+
+    # Initialize saving variables
+    V_sol = zeros(Tdt+1, 6)
+    V_sol[1, :] = copy(Vcur)
+
+    for z = 2 : Tdt+1
+        # STG ODEs
+        for j = 1 : 6
+            # Liu model ODEs
+            Vcur[j] += dV(Vprev[j], mNa[j], hNa[j], mCaT[j], hCaT[j], mCaS[j], hCaS[j], mA[j], hA[j], mKCa[j], mKd[j],
+                          mH[j], Ca[j], Iapp[j], gNa[j], gCaT[j], gCaS[j], gA[j], gKCa[j], gKd[j], gH[j], gleak[j])
+            mKCa[j] += dmKCa(Vprev[j], Ca[j], mKCa[j], tmKCa[j])
+            Ca[j] += dCa(Vprev[j], mCaT[j], hCaT[j], mCaS[j], hCaS[j], Ca[j], gCaT[j], gCaS[j])
+            mNa[j] += dmNa(Vprev[j], mNa[j])
+            hNa[j] += dhNa(Vprev[j], hNa[j])
+            mCaT[j] += dmCaT(Vprev[j], mCaT[j])
+            hCaT[j] += dhCaT(Vprev[j], hCaT[j])
+            mCaS[j] += dmCaS(Vprev[j], mCaS[j])
+            hCaS[j] += dhCaS(Vprev[j], hCaS[j])
+            mA[j] += dmA(Vprev[j], mA[j])
+            hA[j] += dhA(Vprev[j], hA[j])
+            mKd[j] += dmKd(Vprev[j], mKd[j])
+            mH[j] += dmH(Vprev[j], mH[j])
+
+            # Computing new reference values of gCaS and gA if DICs have changed
+            if !((gs_th[z, j] == gs_th[z-1, j]) & (gu_th[z, j] == gu_th[z-1, j]))
+                (gCaS_r[j], gA_r[j]) = DICs_gmax_neuromodCaSA(gNa[j], gCaT[j], gKd[j], gKCa[j], gH[j],
+                                                    gleak[j], gs_th[z, j], gu_th[z, j], Vth[j], tmKCa[j])
+            end
+
+            # Computing control signals
+            uCaS, vCaS, eCaS = uve(gCaS[j], gCaS_r[j], zCaS[j], Kp, Ki, u_maxCaS)
+            uA, vA, eA = uve(gA[j], gA_r[j], zA[j], Kp, Ki, u_maxA)
+
+            # Controller ODEs
+            gCaSi[j] += dgi(gCaS[j], gCaSi[j], α, β, uCaS)
+            gCaS[j] += dg(gCaS[j], gCaSi[j], α)
+            zCaS[j] += dz(eCaS, Kt, uCaS, vCaS)
+            gAi[j] += dgi(gA[j], gAi[j], α, β, uA)
+            gA[j] += dg(gA[j], gAi[j], α)
+            zA[j] += dz(eA, Kt, uA, vA)
+        end
+
+        # Coupling ODEs
+        Vcur[1] += (dt) * (1/C) * (-gsyn21*s21*(Vprev[1]-Vsyn) - gsyn51*s51*(Vprev[1]-Vsyn))
+        s21 += ds(Vprev[2], s21)
+        s51 += ds(Vprev[5], s51)
+
+        Vcur[2] += (dt) * (1/C) * (-gsyn12*s12*(Vprev[2]-Vsyn) - gsyn62*s62*(Vprev[2]-Vsyn))
+        s12 += ds(Vprev[1], s12)
+        s62 += ds(Vprev[6], s62)
+
+        Vcur[3] += (dt) * (1/C) * (-gsyn43*s43*(Vprev[3]-Vsyn) - gsyn53*s53*(Vprev[3]-Vsyn) - gsyn63*s63*(Vprev[3]-Vsyn))
+        s43 += ds(Vprev[4], s43)
+        s53 += ds(Vprev[5], s53)
+        s63 += ds(Vprev[6], s63)
+
+        Vcur[4] += (dt) * (1/C) * (-gsyn34*s34*(Vprev[4]-Vsyn))
+        s34 += ds(Vprev[3], s34)
+
+        Vprev = copy(Vcur)
+        V_sol[z, :] = copy(Vcur)
+    end
+
+    return V_sol
+end
+
 function simulateSTG_network(Iapp, gNa, gCaT, gCaSinit, gAinit, gKCa, gKd, gH, gleak, gsyn,
                              tmKCa, α, β, Kp, Ki, Kt, gsth, guth, Vth, u_maxCaS, u_maxA)
 
--- Quadrupet gait control/quad_STG_models.jl
+++ Quadrupet gait control/quad_STG_models.jl
@@ -45,6 +45,164 @@ function uve(g, g_r, z, Kp, Ki, u_max)
     return u, v, e
 end
 
+function simulateSTG_network_diff_ICs(Iapp, gNa, gCaT, gCaSinit, gAinit, gKCa, gKd, gH, gleak, gsyn,
+                             tmKCa, α, β, Kp, Ki, Kt, gsth, guth, Vth, u_maxCaS, u_maxA, Vprev)
+
+    # Extracting synaptic conductances
+    gsyn12 = gsyn[1]
+    gsyn21 = gsyn[2]
+    gsyn34 = gsyn[3]
+    gsyn43 = gsyn[4]
+    gsyn56 = gsyn[5]
+    gsyn65 = gsyn[6]
+    gsyn78 = gsyn[7]
+    gsyn87 = gsyn[8]
+    gsyn51 = gsyn[9]
+    gsyn53 = gsyn[10]
+    gsyn62 = gsyn[11]
+    gsyn64 = gsyn[12]
+    gsyn72 = gsyn[13]
+    gsyn73 = gsyn[14]
+    gsyn81 = gsyn[15]
+    gsyn84 = gsyn[16]
+
+    # Initial conditions
+    gCaS = copy(gCaSinit)
+    gCaSi = copy(gCaSinit)
+    zCaS = zero(gCaS)
+    gA = copy(gAinit)
+    gAi = copy(gAinit)
+    zA = zero(gA)
+
+    #Vprev = .-60. .* ones(8) .+ 10. .* (rand(8).-0.5)
+    Vcur = copy(Vprev)
+    mNa = mNa_inf.(Vprev)
+    hNa = hNa_inf.(Vprev)
+    mCaT = mCaT_inf.(Vprev)
+    hCaT = hCaT_inf.(Vprev)
+    mCaS = mCaS_inf.(Vprev)
+    hCaS = hCaS_inf.(Vprev)
+    mA = mA_inf.(Vprev)
+    hA = hA_inf.(Vprev)
+    Ca = .-0.94 .* (gCaT .* mCaT.^3 .* hCaT .* (Vprev.-VCa) .+
+                    gCaS .* mCaS.^3 .* hCaS .* (Vprev.-VCa)) .+ 0.05
+    mKCa = mKCa_inf.(Vprev, Ca)
+    mKd = mKd_inf.(Vprev)
+    mH = mH_inf.(Vprev)
+
+    gs_th = zeros(Tdt+1, 8)
+    gu_th = zero(gs_th)
+    for j = 1 : 8
+        gs_th[:, j] = gsth[j].(tsim)
+        gu_th[:, j] = guth[j].(tsim)
+    end
+
+    gCaS_r = zeros(8)
+    gA_r = zeros(8)
+    for j = 1 : 8
+        (gCaS_r[j], gA_r[j]) = DICs_gmax_neuromodCaSA(gNa[j], gCaT[j], gKd[j], gKCa[j], gH[j],
+                                                gleak[j], gs_th[1, j], gu_th[1, j], Vth[j], tmKCa[j])
+    end
+
+    s12 = s_inf(Vprev[1])
+    s21 = s_inf(Vprev[2])
+    s34 = s_inf(Vprev[3])
+    s43 = s_inf(Vprev[4])
+    s56 = s_inf(Vprev[5])
+    s65 = s_inf(Vprev[6])
+    s78 = s_inf(Vprev[7])
+    s87 = s_inf(Vprev[8])
+    s51 = s_inf(Vprev[5])
+    s53 = s_inf(Vprev[5])
+    s62 = s_inf(Vprev[6])
+    s64 = s_inf(Vprev[6])
+    s72 = s_inf(Vprev[7])
+    s73 = s_inf(Vprev[7])
+    s81 = s_inf(Vprev[8])
+    s84 = s_inf(Vprev[8])
+
+    # Initialize saving variables
+    V_sol = zeros(Tdt+1, 8)
+    V_sol[1, :] = copy(Vcur)
+
+    for z = 2 : Tdt+1
+        # STG ODEs
+        for j = 1 : 8
+            # Liu model ODEs
+            Vcur[j] += dV(Vprev[j], mNa[j], hNa[j], mCaT[j], hCaT[j], mCaS[j], hCaS[j], mA[j], hA[j], mKCa[j], mKd[j],
+                          mH[j], Ca[j], Iapp[j], gNa[j], gCaT[j], gCaS[j], gA[j], gKCa[j], gKd[j], gH[j], gleak[j])
+            mKCa[j] += dmKCa(Vprev[j], Ca[j], mKCa[j], tmKCa[j])
+            Ca[j] += dCa(Vprev[j], mCaT[j], hCaT[j], mCaS[j], hCaS[j], Ca[j], gCaT[j], gCaS[j])
+            mNa[j] += dmNa(Vprev[j], mNa[j])
+            hNa[j] += dhNa(Vprev[j], hNa[j])
+            mCaT[j] += dmCaT(Vprev[j], mCaT[j])
+            hCaT[j] += dhCaT(Vprev[j], hCaT[j])
+            mCaS[j] += dmCaS(Vprev[j], mCaS[j])
+            hCaS[j] += dhCaS(Vprev[j], hCaS[j])
+            mA[j] += dmA(Vprev[j], mA[j])
+            hA[j] += dhA(Vprev[j], hA[j])
+            mKd[j] += dmKd(Vprev[j], mKd[j])
+            mH[j] += dmH(Vprev[j], mH[j])
+
+            # Computing new reference values of gCaS and gA if DICs have changed
+            if !((gs_th[z, j] == gs_th[z-1, j]) & (gu_th[z, j] == gu_th[z-1, j]))
+                (gCaS_r[j], gA_r[j]) = DICs_gmax_neuromodCaSA(gNa[j], gCaT[j], gKd[j], gKCa[j], gH[j],
+                                                    gleak[j], gs_th[z, j], gu_th[z, j], Vth[j], tmKCa[j])
+            end
+
+            # Computing control signals
+            uCaS, vCaS, eCaS = uve(gCaS[j], gCaS_r[j], zCaS[j], Kp, Ki, u_maxCaS)
+            uA, vA, eA = uve(gA[j], gA_r[j], zA[j], Kp, Ki, u_maxA)
+
+            # Controller ODEs
+            gCaSi[j] += dgi(gCaS[j], gCaSi[j], α, β, uCaS)
+            gCaS[j] += dg(gCaS[j], gCaSi[j], α)
+            zCaS[j] += dz(eCaS, Kt, uCaS, vCaS)
+            gAi[j] += dgi(gA[j], gAi[j], α, β, uA)
+            gA[j] += dg(gA[j], gAi[j], α)
+            zA[j] += dz(eA, Kt, uA, vA)
+        end
+
+        # Coupling ODEs
+        Vcur[1] += (dt) * (1/C) * (-gsyn21*s21*(Vprev[1]-Vsyn) - gsyn51*s51*(Vprev[1]-Vsyn) - gsyn81*s81*(Vprev[1]-Vsyn))
+        s21 += ds(Vprev[2], s21)
+        s51 += ds(Vprev[5], s51)
+        s81 += ds(Vprev[8], s81)
+
+        Vcur[2] += (dt) * (1/C) * (-gsyn12*s12*(Vprev[2]-Vsyn) - gsyn62*s62*(Vprev[2]-Vsyn) - gsyn72*s72*(Vprev[2]-Vsyn))
+        s12 += ds(Vprev[1], s12)
+        s62 += ds(Vprev[6], s62)
+        s72 += ds(Vprev[7], s72)
+
+        Vcur[3] += (dt) * (1/C) * (-gsyn43*s43*(Vprev[3]-Vsyn) - gsyn53*s53*(Vprev[3]-Vsyn) - gsyn73*s73*(Vprev[3]-Vsyn))
+        s43 += ds(Vprev[4], s43)
+        s53 += ds(Vprev[5], s53)
+        s73 += ds(Vprev[7], s73)
+
+        Vcur[4] += (dt) * (1/C) * (-gsyn34*s34*(Vprev[4]-Vsyn) - gsyn64*s64*(Vprev[4]-Vsyn) - gsyn84*s84*(Vprev[4]-Vsyn))
+        s34 += ds(Vprev[3], s34)
+        s64 += ds(Vprev[6], s64)
+        s84 += ds(Vprev[8], s84)
+
+        Vcur[5] += (dt) * (1/C) * (-gsyn65*s65*(Vprev[5]-Vsyn))
+        s65 += ds(Vprev[6], s65)
+
+        Vcur[6] += (dt) * (1/C) * (-gsyn56*s56*(Vprev[6]-Vsyn))
+        s56 += ds(Vprev[5], s56)
+
+        Vcur[7] += (dt) * (1/C) * (-gsyn87*s87*(Vprev[7]-Vsyn))
+        s87 += ds(Vprev[8], s87)
+
+        Vcur[8] += (dt) * (1/C) * (-gsyn78*s78*(Vprev[8]-Vsyn))
+        s78 += ds(Vprev[7], s78)
+
+        Vprev = copy(Vcur)
+        V_sol[z, :] = copy(Vcur)
+    end
+
+    return V_sol
+end
+
 function simulateSTG_network(Iapp, gNa, gCaT, gCaSinit, gAinit, gKCa, gKd, gH, gleak, gsyn,
                              tmKCa, α, β, Kp, Ki, Kt, gsth, guth, Vth, u_maxCaS, u_maxA)
 
